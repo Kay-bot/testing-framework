8c9171fb7e8039a1a4a3c675bae127e247c25170
KAY S <KS.KWANRUAN@GMAIL.com>
2020-07-14 00:44:01 +1000

    display files

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("Running test");
+const Runner = require("./runner");
+const runner = new Runner();
+
+const run = async () => {
+  const results = await runner.collectFiles(process.cwd());
+  console.log(results);
+};
+
+run();

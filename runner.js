0d7bfdd438df03ca014d6611e4a90a11bfa28d83
KAY S <KS.KWANRUAN@GMAIL.com>
2020-07-14 11:02:01 +1000

    customized pass/err message

--- runner.js
+++ runner.js
@@ -9,7 +9,7 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
-      console.log(chalk.gray(`---- ${file.name}`));
+      console.log(chalk.gray(`---- ${file.shortName}`));
       const beforeEaches = [];
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
@@ -18,10 +18,11 @@ class Runner {
         beforeEaches.forEach((func) => func());
         try {
           fn();
-          console.log(chalk.green(`Ok - ${desc}`));
+          console.log(chalk.green(`\tOk - ${desc}`));
         } catch (err) {
-          console.log(chalk.red(`X - ${desc}`));
-          console.log(chalk.red("\t", err.message));
+          const message = err.message.replace(/\n/g, "\n\t\t");
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red("\t", message));
         }
       };
       try {
@@ -40,7 +41,7 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes(".test.js")) {
-        this.testFiles.push({ name: filepath });
+        this.testFiles.push({ name: filepath, shortName: file });
       } else if (stats.isDirectory()) {
         const childFiles = await fs.promises.readdir(filepath);
 
